5e65090a79fa0e95c173240d2c46c8a382645905
Tom <squid@stormwind.it>
2026-06-24 16:54:00 +0200

    docs(tutorials): add caching tutorial and sample

---
+++ samples/SquidStd.Samples.Caching/Program.cs
@@ -0,0 +1,27 @@
+using SquidStd.Caching.Abstractions.Interfaces;
+using SquidStd.Caching.Extensions;
+using SquidStd.Core.Data.Bootstrap;
+using SquidStd.Services.Core.Services.Bootstrap;
+
+var bootstrap = SquidStdBootstrap.Create(new SquidStdOptions
+{
+    ConfigName = "squidstd",
+    RootDirectory = AppContext.BaseDirectory
+});
+
+#region step-1
+bootstrap.ConfigureServices(container => container.AddInMemoryCache());
+#endregion
+
+await bootstrap.StartAsync();
+
+#region step-2
+var cache = bootstrap.Resolve<ICacheService>();
+
+await cache.SetAsync("greeting", "hello", TimeSpan.FromMinutes(5));
+var value = await cache.GetAsync<string>("greeting");
+
+Console.WriteLine(value);
+#endregion
+
+await bootstrap.StopAsync();
