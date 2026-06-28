ee197da992bb2d0be8caf1555ff7ae1b47ecbe62
Tom <squid@stormwind.it>
2026-06-28 14:28:24 +0200

    feat(crypto): add RegisterPgp DI extension

---
+++ src/SquidStd.Crypto/Pgp/Extensions/RegisterPgpExtensions.cs
@@ -0,0 +1,30 @@
+using DryIoc;
+using SquidStd.Crypto.Pgp.Interfaces;
+using SquidStd.Crypto.Pgp.Services;
+
+namespace SquidStd.Crypto.Pgp.Extensions;
+
+/// <summary>DryIoc registration helpers for the PGP module.</summary>
+public static class RegisterPgpExtensions
+{
+    /// <param name="container">Container that receives the PGP registrations.</param>
+    extension(IContainer container)
+    {
+        /// <summary>
+        ///     Registers the PGP keyring, service, and the chosen key store (all singletons). The keyring is not
+        ///     auto-loaded; call <see cref="IPgpKeyring.LoadAsync" /> at startup if persistence is desired.
+        /// </summary>
+        /// <param name="keyStoreFactory">Builds the <see cref="IPgpKeyStore" /> from the resolver.</param>
+        /// <returns>The same container for chaining.</returns>
+        public IContainer RegisterPgp(Func<IResolver, IPgpKeyStore> keyStoreFactory)
+        {
+            ArgumentNullException.ThrowIfNull(keyStoreFactory);
+
+            container.Register<IPgpKeyring, PgpKeyring>(Reuse.Singleton);
+            container.Register<IPgpService, PgpService>(Reuse.Singleton);
+            container.RegisterDelegate(keyStoreFactory, Reuse.Singleton);
+
+            return container;
+        }
+    }
+}
